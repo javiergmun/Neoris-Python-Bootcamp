74e4b033361ff62b0a3aa75bac9fe6c96a66eea9
javiergmun <javiergmun@gmail.com>
2022-04-04 14:12:18 +0200

    Operadores exercise

---
+++ Operadores/Ejercicio.py
@@ -0,0 +1,13 @@
+edad_hermano=24
+print("Tu edad es: ", edad_hermano)
+edad2=int (edad_hermano/2)
+print("Edad entre dos: ", edad2)
+print("-----------------------------------")
+edad_hermana=6
+edad_real_hermana=edad_hermana+15
+print("La edad de la hermana es: ", edad_real_hermana)
+print("-----------------------------------")
+if edad_hermano > edad_real_hermana :
+    print("Hermano es mayor")
+else:
+    print("Hermana es mayor")
